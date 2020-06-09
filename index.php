328a3dc7c42026d922105ab36d2b14632ceb127f
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 15:41:07 +0200

    correction

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
+<?php 
+    $age = 21;
+    $gender = 'Homme';
+    if ($gender == 'Homme' && $age < 18 && $age > 0) {
+        $message = 'Vous êtes un homme et vous êtes mineur';
+    }else if ($gender == 'Homme' && $age >= 18 && $age < 125) {
+        $message = 'Vous êtes un homme et vous êtes majeur';
+    }else if ($gender == 'Femme' && $age < 18 && $age > 0) {
+        $message = 'Vous êtes une femme et vous êtes mineure';
+    }else if ($gender == 'Femme' && $age >= 18 && $age < 125) {
+        $message = 'Vous êtes une femme et vous êtes majeure';
+    }else {
+        $message = 'Votre âge ou votre genre n\'est pas valide';
+    }
+?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>partie 2 exercice 3</title>
-</head>
-<body>
-    <p>
-        <?php
-        $age = 5;
-        $gender = 'Femme';
-        if ($gender == 'Homme' && $age >= 18 ){
-            echo 'Vous êtes un homme et vous êtes majeur';
-        }else if ($gender == 'Homme' && $age < 18){
-            echo 'Vous êtes un homme et vous êtes mineur';
-        }else if ($gender == 'Femme' && $age >= 18){
-            echo 'Vous êtes une femme et vous êtes majeure';
-        }else if ($gender == 'Femme' && $age < 18){
-            echo 'Vous êtes une femme et vous êtes mineur';
-        }
-        ?>
-    </p>
-</body>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Partie 2 exercice 3</title>
+    </head>
+    <body>
+        <p><?= $message ?></p>
+    </body>
 </html>
